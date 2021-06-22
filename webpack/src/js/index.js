2ffb948616229367e17c15f3935b557c34899f18
Pyro(고정완) <rhwjddhks@gmail.com>
2021-06-23 08:13:32 +0900

    feat: css 로더를 설정

--- webpack/src/js/index.js
+++
@@ -1,7 +0,0 @@
-// Entry File
-
-import hello_word from "./hello.js";
-import world_word from "./world.js";
-
-const word = `${hello_word} ${world_word}`;
-document.querySelector("#root").innerHTML = word;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,9 +2,20 @@ const path = require("path");
 
 module.exports = {
     mode: "production",
-    entry: "./src/js/index.js",
+    entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "src/dist"),
+        path: path.resolve(__dirname, "./src/dist"),
         filename: "index_bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
     }
 }
